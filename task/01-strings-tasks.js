56be0c7b31eb14c50f0e7300be58f51f5bd49202
CrazyTapok <masyanya0772@gmail.com>
2019-02-28 21:37:05 +0300

    Update 01-strings-tasks.js
    hel

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -172,7 +172,8 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-  throw new Error('Not implemented');
+  str.replace(";", "', '");
+  return "[" + str + "]";
 }
 
 /**
